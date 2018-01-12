53b597a9ba2baab540c797e4e1c5de660bd09533
philostene <gspillette@gmail.com>
2018-01-12 14:33:09 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string chaineNormale = "Bonjour à tous";
+        string chaineCryptee = Encodage.Crypte(chaineNormale);
+        Console.WriteLine(chaineCryptee);
+        chaineNormale = Encodage.Decrypte(chaineCryptee);
+        Console.WriteLine(chaineNormale);
+        Console.ReadKey();
+    }
+
+}
+
+public static class Encodage
+{
+    public static string Crypte(string chaine)
+    {
+        return Convert.ToBase64String(Encoding.Default.GetBytes(chaine));
+    }
+
+    public static string Decrypte(string chaine)
+    {
+        return Encoding.Default.GetString(Convert.FromBase64String(chaine));
+    }
+}
